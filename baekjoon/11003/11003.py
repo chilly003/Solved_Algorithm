975a23d9488dbe29e3b66fc45f5993a4b0fb2ebe
chilly003 <chfhchf03@naver.com>
2025-04-02 21:18:41 +0900

    .

---
+++ baekjoon/11003/11003.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open("11003.txt")
+from collections import deque
+
+N , L = map(int, input().split())
+now = list(map(int, input().split()))
+mydeque = deque()
+
+for i in range(N):
+    while mydeque and mydeque[-1][0] > now[i]:
+        mydeque.pop()
+    mydeque.append((now[i],i))
+    if mydeque[0][1] <= i - L:
+        mydeque.popleft()
+    print(mydeque[0][0], end=" ")
