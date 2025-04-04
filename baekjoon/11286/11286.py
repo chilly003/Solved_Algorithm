603e693ee92c6fef7f58482b2c5668ebe6f39821
chilly003 <chfhchf03@naver.com>
2025-04-04 17:36:17 +0900

    .

--- baekjoon/11286/11286.py
+++ baekjoon/11286/11286.py
@@ -5,19 +5,7 @@ sys.stdin = open('11286.txt')
 arr = deque()
 
 N = int(input())
-min_int = float("inf")
 
 for _ in range(N):
-    now = int(input())
-    
-    if now != 0:
-        if abs(now) <= min_int:
-            if now < min_int:
-                min_int = now
-        arr.append(now)
-    # 0인 경우
-    else:
-        if arr: 
-            print(arr.pop(min_int))
-        else:
-            print(0)
+    arr.append(int(input()))
+    min_now = 
